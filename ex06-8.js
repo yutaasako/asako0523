61a9bc13704fb7e6a40a8acbb4a9e7c0e887d39e
zm17002 <zm17002@josai.ac.jp>
2018-07-29 21:21:11 -0400

    message

---
+++ ex06-8.js
@@ -0,0 +1,6 @@
+function sayhello68()
+{alert('Hello,'+document.getElementById('who').innerHTML+'!');}
+function taro()
+{documentment.getElementById('who').innerHTML='太郎';}
+function hanako()
+{document.getElementById('who').innerHTML='花子'}
---
+++ ex6-5.js
@@ -0,0 +1,4 @@
+function sayhello()
+{var who='world';alert('Hello,'+who+'!');}
+function taro(){who='太郎';}
+function hanako(){who='花子';}
